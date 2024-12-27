87414c1e947f90f829a9181fc67fd1aecf4bc855
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 02:26:56 +0500

    Task1

---
+++ Tyuiu.VumaR.Sprint4.Task1.V22.Test/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VumaR.Sprint4.Task1.V22.Lib;
+namespace Tyuiu.VumaR.Sprint4.Task1.V22.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] nums = { 1, 2, 3 };
+            int res = ds.Calculate(nums);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.VumaR.Sprint4.Task1.V22/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.VumaR.Sprint4.Task1.V22.Lib;
+
+namespace Tyuiu.VumaR.Sprint4.Task1.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элементов массива:");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("Введите значение " + i + " элементов массива: ");
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив:");
+            for (int i = 0; i <= len - 1; ++i)
+            {
+                Console.Write(numsArray[i] + " \t " + " \t ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+
+            Console.WriteLine("Произведение нечетных элементов массива равна: " + res); ;
+        }
+    }
+}
